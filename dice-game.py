ec3a33e4db682d8fc5d7965632fb10472e20ab0f
Mostafa Al Mamun Fahim <67799909+pathetic-coder@users.noreply.github.com>
2023-09-06 00:35:48 +0600

    added sum of dice

--- dice-game.py
+++ dice-game.py
@@ -1,6 +1,7 @@
 import random as randi
 
 def dice(amount:int):
+    sum = 0
    
     if amount<=0:
         raise ValueError
@@ -9,6 +10,7 @@ def dice(amount:int):
     for i in range(amount):
         r = randi.randint(1, 6)
         rolls[i] = r
+        sum = sum+r
     
     return rolls
     
@@ -16,13 +18,19 @@ def dice(amount:int):
 def main():
     while True:
         try:
+            print()
             user_input = input("How many dice you want to roll?")
 
             if user_input.lower()=="exit":
                 print("Thanks for playing!!!")
                 break
+            d = dice(int(user_input))
+            s = sum(d)
+            print()
+            print(*d, sep=", ")
+            print("Total sum of dices:", s)
+            print()
             
-            print(dice(int(user_input)))
         except ValueError:
             print("Please enter a valid number!!!")
     
